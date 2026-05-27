7e83276caa09482a14693bd4f4c4c8bbe7405741
Kevin L <laikevin279@gmail.com>
2026-05-28 07:20:31 +0800

    corrections

--- ISP.v
+++ ISP.v
@@ -1,4 +1,3 @@
-
 module ISP(
     //Input Port
     clk,
@@ -31,21 +30,17 @@ output reg [11:0] r_out;
 output reg [11:0] g_out;
 output reg [11:0] b_out;
 
-//==============================
-//   Design
-//==============================
-
 localparam IMG_W = 16;
 localparam IMG_H = 16;
 localparam TOTAL_PIXELS = 256;
 localparam TH = 320;
 localparam BLACK_LEVEL = 64;
 
-localparam S_IDLE = 3'd0;
-localparam S_INPUT = 3'd1;
-localparam S_LSC = 3'd2;
-localparam S_DPC = 3'd3;
-localparam S_OUTPUT = 3'd4;
+localparam S_IDLE     = 3'd0;
+localparam S_INPUT    = 3'd1;
+localparam S_LSC      = 3'd2;
+localparam S_DPC      = 3'd3;
+localparam S_OUTPUT   = 3'd4;
 
 reg [2:0] state;
 reg [7:0] in_cnt;
@@ -57,35 +52,36 @@ reg [11:0] raw_img [0:255];
 reg [11:0] lsc_img [0:255];
 reg [11:0] dpc_img [0:255];
 
-reg [11:0] gain_r [0:35];
+reg [11:0] gain_r  [0:35];
 reg [11:0] gain_gr [0:35];
 reg [11:0] gain_gb [0:35];
-reg [11:0] gain_b [0:35];
+reg [11:0] gain_b  [0:35];
+
 
 function [3:0] get_x;
     input [7:0] idx;
-    begin 
+    begin
         get_x = idx[3:0];
-    end 
-endfunction 
+    end
+endfunction
 
 function [3:0] get_y;
     input [7:0] idx;
-    begin 
+    begin
         get_y = idx[7:4];
-    end 
-endfunction 
+    end
+endfunction
 
 function integer reflect16;
     input integer v;
-    begin 
+    begin
         if (v < 0)
             reflect16 = -v;
         else if (v >= 16)
             reflect16 = 30 - v;
-        else 
+        else
             reflect16 = v;
-    end 
+    end
 endfunction
 
 function [7:0] idx_reflect;
@@ -93,34 +89,34 @@ function [7:0] idx_reflect;
     input integer y;
     integer xr;
     integer yr;
-    begin 
+    begin
         xr = reflect16(x);
         yr = reflect16(y);
         idx_reflect = yr * 16 + xr;
-    end 
+    end
 endfunction
 
 function [1:0] bayer_type;
     input integer x;
     input integer y;
-    begin 
+    begin
         if ((y % 2) == 0 && (x % 2) == 0)
-            bayer_type = 2'd0;
+            bayer_type = 2'd0;       
         else if ((y % 2) == 0 && (x % 2) == 1)
-            bayer_type = 2'd1;
-        else if ((y % 2) == 1 && (X % 2) == 0)
-            bayer_type = 2'd2;
-        else 
-            bayer_type = 2'd3;
+            bayer_type = 2'd1;      
+        else if ((y % 2) == 1 && (x % 2) == 0)
+            bayer_type = 2'd2;       
+        else
+            bayer_type = 2'd3;       
     end
-endfunction 
+endfunction
 
 function [11:0] get_gain;
     input [1:0] ch;
     input integer row;
     input integer col;
     integer gidx;
-    begin gidx = row * 6 + col;
+    begin
         gidx = row * 6 + col;
         case (ch)
             2'd0: get_gain = gain_r[gidx];
@@ -133,19 +129,19 @@ endfunction
 
 function [11:0] clip12;
     input integer v;
-    begin 
+    begin
         if (v < 0)
-            clip12 = 12'd0;.
+            clip12 = 12'd0;
         else if (v > 4095)
             clip12 = 12'd4095;
-        else 
+        else
             clip12 = v[11:0];
     end
 endfunction
 
 function integer abs_int;
     input integer a;
-    begin 
+    begin
         if (a < 0)
             abs_int = -a;
         else
@@ -157,7 +153,7 @@ function [11:0] avg2;
     input [11:0] a;
     input [11:0] b;
     integer t;
-    begin 
+    begin
         t = a + b;
         avg2 = t >> 1;
     end
@@ -198,7 +194,7 @@ function integer sad4;
     input [11:0] c;
     input [11:0] d;
     input [11:0] med;
-    begin 
+    begin
         sad4 = abs_int(a - med) + abs_int(b - med) + abs_int(c - med) + abs_int(d - med);
     end
 endfunction
@@ -216,29 +212,29 @@ function [11:0] calc_lsc_pixel;
     reg [1:0] ch;
     reg [11:0] g00, g01, g10, g11;
     reg [11:0] interp_gain;
-    begin 
+    begin
         x = get_x(idx);
         y = get_y(idx);
 
         x0 = x / 3;
         y0 = y / 3;
         if (x0 > 4) x0 = 4;
-        if (y0 > 4) y0 = 4; 
-        
+        if (y0 > 4) y0 = 4;
+
         rx = x - 3 * x0;
         ry = y - 3 * y0;
         if (rx > 2) rx = 2;
         if (ry > 2) ry = 2;
 
         dx = (rx * 256 + 1) / 3;
-        ix = 256 - dx
+        ix = 256 - dx;
         dy = (ry * 256 + 1) / 3;
         iy = 256 - dy;
 
         ch = bayer_type(x, y);
-        g00 = get_gain(ch, y0, x0);
-        g01 = get_gain(ch, y0, x0 + 1);
-        g10 = get_gain(ch, y0+1, x0);
+        g00 = get_gain(ch, y0,     x0);
+        g01 = get_gain(ch, y0,     x0 + 1);
+        g10 = get_gain(ch, y0 + 1, x0);
         g11 = get_gain(ch, y0 + 1, x0 + 1);
 
         sum_gain = g00 * ix * iy + g10 * ix * dy + g01 * dx * iy + g11 * dx * dy + 32768;
@@ -246,30 +242,29 @@ function [11:0] calc_lsc_pixel;
 
         if (raw_img[idx] > BLACK_LEVEL)
             raw_minus_blc = raw_img[idx] - BLACK_LEVEL;
-        else 
+        else
             raw_minus_blc = 0;
 
         final_v = (raw_minus_blc * interp_gain + 512) >> 10;
         calc_lsc_pixel = clip12(final_v);
-    end 
+    end
 endfunction
-
 
 function [11:0] calc_dpc_pixel;
     input [7:0] idx;
     integer x, y;
     reg [11:0] p;
-    reg[11:0] h0, h1, h2, h3;
-    reg[11:0] v0, v1, v2, v3;
-    reg[11:0] d10, d11, d12, d13;
-    reg[11:0] d20, d21, d22, d23;
-    reg[11:0] mh, mv, md1, md2;
+    reg [11:0] h0, h1, h2, h3;
+    reg [11:0] v0, v1, v2, v3;
+    reg [11:0] d10, d11, d12, d13;
+    reg [11:0] d20, d21, d22, d23;
+    reg [11:0] mh, mv, md1, md2;
     integer sh, sv, sd1, sd2;
     reg [11:0] target;
     begin
         x = get_x(idx);
         y = get_y(idx);
-        p = lsc_img(idx);
+        p = lsc_img[idx];
 
         h0 = lsc_img[idx_reflect(x - 2, y)];
         h1 = lsc_img[idx_reflect(x - 1, y)];
@@ -291,18 +286,19 @@ function [11:0] calc_dpc_pixel;
         d22 = lsc_img[idx_reflect(x + 1, y - 1)];
         d23 = lsc_img[idx_reflect(x + 2, y - 2)];
 
-        mh = median4(h0, h1, h2, h3);
-        mv = median4(v0, v1, v2, v3);
+        mh  = median4(h0, h1, h2, h3);
+        mv  = median4(v0, v1, v2, v3);
         md1 = median4(d10, d11, d12, d13);
         md2 = median4(d20, d21, d22, d23);
 
-        sh = sad4(h0, h1, h2, h3, mh);
-        sv = sad4(v0, v1, v2, v3, mv);
+        sh  = sad4(h0, h1, h2, h3, mh);
+        sv  = sad4(v0, v1, v2, v3, mv);
         sd1 = sad4(d10, d11, d12, d13, md1);
         sd2 = sad4(d20, d21, d22, d23, md2);
 
-        Target = mh;
-        if (sh < sv) begin
+        // Tie-breaking priority: H > V > D1 > D2
+        target = mh;
+        if (sv < sh) begin
             target = mv;
             sh = sv;
         end
@@ -314,9 +310,9 @@ function [11:0] calc_dpc_pixel;
             target = md2;
         end
 
-        if (abs_int(p-target) > TH)
-            calc_dpc_pixel = target;  
-        else 
+        if (abs_int(p - target) > TH)
+            calc_dpc_pixel = target;
+        else
             calc_dpc_pixel = p;
     end
 endfunction
@@ -325,7 +321,7 @@ function [11:0] get_dpc;
     input integer x;
     input integer y;
     begin
-        get_dpc = dpc_img[idx_reflect(x,y)];
+        get_dpc = dpc_img[idx_reflect(x, y)];
     end
 endfunction
 
@@ -334,8 +330,8 @@ function [11:0] ccm_r;
     input [11:0] gg;
     input [11:0] bb;
     integer t;
-    begin t = 1100 * rr - 50 * gg - 50 * bb + 512;
-        t = 1100 * rr - 50 * gg -50 * bb + 512;
+    begin
+        t = 1100 * rr - 50 * gg - 50 * bb + 512;
         t = t >>> 10;
         ccm_r = clip12(t);
     end
@@ -346,8 +342,7 @@ function [11:0] ccm_g;
     input [11:0] gg;
     input [11:0] bb;
     integer t;
-
-    begin 
+    begin
         t = -50 * rr + 1100 * gg - 50 * bb + 512;
         t = t >>> 10;
         ccm_g = clip12(t);
@@ -359,7 +354,7 @@ function [11:0] ccm_b;
     input [11:0] gg;
     input [11:0] bb;
     integer t;
-    begin 
+    begin
         t = -50 * rr - 50 * gg + 1100 * bb + 512;
         t = t >>> 10;
         ccm_b = clip12(t);
@@ -371,21 +366,22 @@ function [35:0] calc_rgb_pixel;
     integer x, y;
     reg [11:0] n, s, e, w, nw, ne, sw, se, c;
     reg [11:0] rr, gg, bb;
-    reg [11:0 ] ro, go, bo;
+    reg [11:0] ro, go, bo;
     reg [1:0] typ;
-    begin 
+    begin
         x = get_x(idx);
         y = get_y(idx);
-        n = get_dpc(x, y - 1);
-        s = get_dpc(x, y + 1);
-        e = get_dpc(x + 1, y);
-        w = get_dpc(x -1, y);
+        c  = get_dpc(x, y);
+        n  = get_dpc(x, y - 1);
+        s  = get_dpc(x, y + 1);
+        e  = get_dpc(x + 1, y);
+        w  = get_dpc(x - 1, y);
         nw = get_dpc(x - 1, y - 1);
         ne = get_dpc(x + 1, y - 1);
         sw = get_dpc(x - 1, y + 1);
         se = get_dpc(x + 1, y + 1);
         typ = bayer_type(x, y);
-        
+
         case (typ)
             2'd0: begin
                 rr = c;
@@ -405,15 +401,15 @@ function [35:0] calc_rgb_pixel;
             default: begin
                 rr = avg2(n, s);
                 gg = c;
-                bb = av2(w, e);
+                bb = avg2(w, e);
             end
         endcase
 
         ro = ccm_r(rr, gg, bb);
         go = ccm_g(rr, gg, bb);
         bo = ccm_b(rr, gg, bb);
-        
-        calc_rgb_pixel = {ro, go , bo};
+
+        calc_rgb_pixel = {ro, go, bo};
     end
 endfunction
 
@@ -429,7 +425,7 @@ always @(posedge clk or negedge rst_n) begin
         param_cnt <= 8'd0;
         out_valid <= 1'b0;
         r_out <= 12'd0;
-        g_out = <= 12'd0;
+        g_out <= 12'd0;
         b_out <= 12'd0;
     end else begin
         out_valid <= 1'b0;
@@ -439,18 +435,20 @@ always @(posedge clk or negedge rst_n) begin
 
         if (param_valid) begin
             if (param_cnt < 8'd36)
-                gain_r(param_cnt) <= param_gain;
-            else if (param_cnt - 8'd72) <= param_gain;
-                gain_gr[param_cnt - 8'd72] <= param_gain;
-            else if (param_cnt - 8'd108)
+                gain_r[param_cnt] <= param_gain;
+            else if (param_cnt < 8'd72)
+                gain_gr[param_cnt - 8'd36] <= param_gain;
+            else if (param_cnt < 8'd108)
                 gain_gb[param_cnt - 8'd72] <= param_gain;
+            else
+                gain_b[param_cnt - 8'd108] <= param_gain;
 
             if (param_cnt != 8'd143)
                 param_cnt <= param_cnt + 8'd1;
-        end 
+        end
 
         case (state)
-            S_IDLE: begin  
+            S_IDLE: begin
                 if (in_valid) begin
                     raw_img[8'd0] <= in;
                     in_cnt <= 8'd1;
@@ -480,6 +478,7 @@ always @(posedge clk or negedge rst_n) begin
                     calc_cnt <= calc_cnt + 8'd1;
                 end
             end
+
             S_DPC: begin
                 dpc_img[calc_cnt] <= calc_dpc_pixel(calc_cnt);
                 if (calc_cnt == 8'd255) begin
@@ -491,4 +490,26 @@ always @(posedge clk or negedge rst_n) begin
                 end
             end
 
-    end 
+            S_OUTPUT: begin
+                rgb_pack = calc_rgb_pixel(out_cnt);
+                out_valid <= 1'b1;
+                r_out <= rgb_pack[35:24];
+                g_out <= rgb_pack[23:12];
+                b_out <= rgb_pack[11:0];
+
+                if (out_cnt == 8'd255) begin
+                    out_cnt <= 8'd0;
+                    state <= S_IDLE;
+                end else begin
+                    out_cnt <= out_cnt + 8'd1;
+                end
+            end
+
+            default: begin
+                state <= S_IDLE;
+            end
+        endcase
+    end
+end
+
+endmodule
